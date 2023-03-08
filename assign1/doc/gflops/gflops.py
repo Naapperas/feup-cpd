45b1304d124ffc2969c4106641195754dcccd6ce
Nuno Pereira <nunoafonso2002@gmail.com>
2023-03-08 09:30:43 +0000

    Updated gflop script

--- assign1/doc/gflops/gflops.py
+++ assign1/doc/gflops/gflops.py
@@ -13,19 +13,25 @@ with open(output_file, "w") as f_out:
             gflops = []
             sizes = []
             times = []
+            dcm1s = []
+            dcm2s = []
 
             for line in lines:
-                parsed = line.split()
+                parsed = line.split(' - ')
                 size = int(parsed[0])
-                time = float(parsed[2])
-                dcm1 = int(parsed[4])
-                dcm2 = int(parsed[6])
+                time = float(parsed[1])
+                dcm1 = int(parsed[2])
+                dcm2 = int(parsed[3])
+
+                print(size, time, dcm1, dcm2)
 
                 gflop = 2 * (size**3) / time
 
                 gflops.append(gflop)
                 sizes.append(size)
                 times.append(time)
+                dcm1s.append(dcm1)
+                dcm2s.append(dcm2)
 
             # Extract the base filename without the extension
             base_filename = os.path.splitext(filename)[0]
@@ -35,6 +41,6 @@ with open(output_file, "w") as f_out:
             f_out.write("| Matrix size | Execution time(s) | GFLOPS | L1 Data Cache Misses | L2 Data Cache Misses |\n")
             f_out.write("| ----------- | ----------------- | ------ | -------------------- | -------------------- |\n")
             for i in range(len(gflops)):
-                f_out.write(f"| {sizes[i]} | {times[i]:.3f} | {gflops[i]:.3f} | {dcm1} | {dcm2} |\n")
+                f_out.write(f"| {sizes[i]} | {times[i]:.3f} | {gflops[i]:.3f} | {dcm1s[i]} | {dcm2s[i]} |\n")
 
         f_out.write("\n")
