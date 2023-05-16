2fd4f5cfcb8d1b061a6f2f2e37dc170d8555a04f
Guilherme Almeida <me@gui1612.com>
2023-05-16 01:01:10 +0100

    feat: adds Game placeholder class

---
+++ assign2/src/Game.java
@@ -0,0 +1,13 @@
+import java.net.Socket;
+import java.util.List;
+
+public class Game implements Runnable {
+    public Game(int maxPlayersPerGame, List<Socket> gamePlayers) {
+        System.out.println("TODO: GAME MISSING KEKEKEKEKEKEK");
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
